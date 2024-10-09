5d04a1c1e5a8bda13484341e5ff4fc8fe1a8e0f5
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-10-09 10:36:03 -0300

    Increasing other menuPage.js selectors

--- cypress/pages/menuPage.js
+++ cypress/pages/menuPage.js
@@ -1,8 +1,19 @@
 class MenuPage {
     selectorsList() {
         const selectors = {
+            adminButton: '[href="/web/index.php/admin/viewSystemUsers"]',
+            pimButton: '[href="/web/index.php/pim/viewEmployeeList"]',
+            leaveButton: '[href="/web/index.php/leave/viewLeaveList"]',
+            timeButton: '[href="/web/index.php/time/viewEmployeeTimesheet"]',
+            recruitmentButton: '[href="/web/index.php/recruitment/viewCandidates"]',
             myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-            performanceButton: '[href="/web/index.php/performance/viewPerformanceModule"]'
+            performanceButton: '[href="/web/index.php/performance/searchEvaluatePerformanceReview"]',
+            dashboardButton: '[href="/web/index.php/dashboard/index"]',
+            directoryButton: '[href="/web/index.php/directory/viewDirectory"]',
+            maintenanceButton: '[href="/web/index.php/maintenance/purgeEmployee"]',
+            claimButton: '[href="/web/index.php/claim/viewAssignClaim"]',
+            buzzButton: '[href="/web/index.php/buzz/viewBuzz"]',
+
         }
         return selectors
 
@@ -12,7 +23,51 @@ class MenuPage {
     }
 
     accessPerformance() {
-        cy.get(this.selectorsList().myInfoButton).click()
+        cy.get(this.selectorsList().performanceButton).click()
+    }
+
+    accessAdmin() {
+        cy.get(this.selectorsList().adminButton).click()
+    }
+
+    acessPIM() {
+        cy.get(this.selectorsList().pimButton).click()
+    }
+
+    accessLeave() {
+        cy.get(this.selectorsList().leaveButton).click()
+    }
+
+    accessTime() {
+        cy.get(this.selectorsList().timeButton).click()
+    }
+
+    accessRecruitment() {
+        cy.get(this.selectorsList().recruitmentButton).click()
+    }
+
+    accessPerformance() {
+        cy.get(this.selectorsList().performanceButton).click()
+    }
+
+    accessDashboard() {
+        cy.get(this.selectorsList().dashboardButton).click()
+    }
+
+    accessDiretory() {
+        cy.get(this.selectorsList().directoryButton).click()
+    }
+
+    accessMaintenance() {
+        cy.get(this.selectorsList().maintenanceButton).click()
+    }
+    
+    accessClaim() {
+        cy.get(this.selectorsList().claimButton).click()
+    }
+        
+    accessBuzz() {
+        cy.get(this.selectorsList().buzzButton).click()
     }
 }
 
